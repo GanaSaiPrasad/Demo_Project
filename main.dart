89a96613e5ece3efaebcddb74d5794c919802712
GanaSaiPrasad <sai2k.idea@gmail.com>
2021-08-18 06:59:03 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+// import 'package:mobilePlayGround1/Form/Personal%20Info.dart';
+// import 'package:mobilePlayGround1/PersonalExpensesApp/expensesapp.dart';
+// import 'package:mobilePlayGround1/PlacesApp/great_places_app.dart';
+// import 'package:mobilePlayGround1/ShoppingApp/shopApp.dart';
+import 'package:mobilePlayGround1/ChatAPP/chat_app.dart';
+import 'package:mobilePlayGround1/PlacesApp/great_places_app.dart';
+
+//import 'package:mobilePlayGround1/PersonalExpensesApp/expensesapp.dart';
+//import 'package:mobilePlayGround1/QuizApp/simple.dart';
+
+void main()
+// async
+{
+  // runApp(MealsApp());
+  //runApp(ExpensesApp());
+
+  // // runApp(ShopApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(GreatPlacesApp());
+
+  //runApp(Home());
+  //runApp(RegisterPage());
+}
